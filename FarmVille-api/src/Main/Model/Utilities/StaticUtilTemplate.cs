36e784983b5facaea36bfbcd667165baeb2b219a
Alen Van <ajv6049@rit.edu>
2022-08-21 12:29:13 -0400

    recommit

---
+++ FarmVille-api/src/Main/Model/Utilities/StaticUtilTemplate.cs
@@ -0,0 +1,12 @@
+namespace FarmVille_api.src.Main.Model.Utilities
+{
+    public static class StaticUtilTemplate
+    {
+        public static string token = "";
+        public static string seedsJson = "";
+        public static string plantsJson = "";
+        public static string playersJson = "";
+        public static string shopJson = "";
+
+    }
+}
